c4e18a8aa9ed09b6ea6630631e3d8b28c79c13ef
Oryna Kubarieva <131910000+OrynaKub@users.noreply.github.com>
2026-03-05 23:51:33 -0500

    Refactor i18n to use getRequestConfig for locales

--- i18n.ts
+++ i18n.ts
@@ -1,3 +1,11 @@
-export const locales = ['en', 'fr'] as const;
-export const localePrefix = 'always'; 
-export const defaultLocale = 'en';
+import {getRequestConfig} from 'next-intl/server';
+
+export default getRequestConfig(async ({requestLocale}) => {
+  let locale = await requestLocale;
+
+  if (!locale) locale = 'en';
+
+  return {
+    messages: (await import(`./messages/${locale}.json`)).default
+  };
+});
